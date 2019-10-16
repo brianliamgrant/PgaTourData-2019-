e0fb7ce5a2e3a386d4c3f75745eb5c084e6e94d1
brianliamgrant <49319266+brianliamgrant@users.noreply.github.com>
2019-10-16 00:21:49 -0400

    Update pgaEarnings.py

--- pgaEarnings.py
+++ pgaEarnings.py
@@ -1,14 +1,19 @@
+#use get request method to obtain html from the url
 import requests
 pga = requests.get("http://www.espn.com/golf/statistics/_/year/2019")
 pga
 
+#check to see if status code = 200
 pga.status_code
 
+#import the BeautifulSoup library to parse through the HTML
 from bs4 import BeautifulSoup
 page = (pga.content, 'html.parser')
 
+#re-format the HTML to see the content better
 print(soup.prettify())
 
+#earnings and player data contained in a table in the HTML file and parsed through to get the information in each row
 trElements = soup.find_all('tr')
 for i in trElements:
     print(i.get_text())
